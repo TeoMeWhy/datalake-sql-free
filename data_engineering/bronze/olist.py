578c359f81dd35990fa0f54a0f4673dc12cd3081
teomewhy@gmail.com <teomewhy@gmail.com>
2022-12-10 23:42:30 +0000

    Ingestão para bronze_olist

---
+++ data_engineering/bronze/olist.py
@@ -0,0 +1,23 @@
+# Databricks notebook source
+def ingestion_table(file_path):
+
+    table_name = (file_path.split("/")[-1]
+                               .replace("olist_", "")
+                               .replace(".csv", "")
+                               .replace("_dataset", ""))
+    (spark.read
+          .csv(file_path, header=True)
+          .coalesce(1)
+          .write
+          .mode("overwrite")
+          .format("delta")
+          .option("overwriteSchema", "true")
+          .saveAsTable(f"bronze_olist.{table_name}"))
+
+# COMMAND ----------
+
+file_paths = [ i.path for i in dbutils.fs.ls("/mnt/datalake/raw/olist")]
+
+for f in file_paths:
+    print(f)
+    ingestion_table(f)
